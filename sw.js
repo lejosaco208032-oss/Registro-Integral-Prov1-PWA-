646c89a7ba801a88b6a06107bc77f8be5d4bb96c
lejosaco208032-oss <lejosaco208032@gmail.com>
2026-04-23 00:38:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// [NUEVA FUNCIÓN: SERVICE_WORKER_CORE] - INICIO
+const CACHE_NAME = 'registro-pro-v3.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+// [NUEVA FUNCIÓN] - FIN
